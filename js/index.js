614599f1b109d02e33b31e5230f61414fa54b0f6
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Pro.local>
2023-01-26 12:27:07 +0530

    dynamic cration of blog card and blog container

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-console.log("connected");
+const blogArray = [
+  {
+    id: "0",
+    title: "Blog Title",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/004/815/622/original/software-development-programming-language-coding-3d-isometric-laptop-computer-with-digital-application-isolated-on-white-background-design-vector.jpg",
+    descruption: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+    {
+        id: "1",
+        title: "Blog Title",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/004/815/622/original/software-development-programming-language-coding-3d-isometric-laptop-computer-with-digital-application-isolated-on-white-background-design-vector.jpg",
+        descruption: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+    {
+        id: "2",
+        title: "Blog Title",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/004/815/622/original/software-development-programming-language-coding-3d-isometric-laptop-computer-with-digital-application-isolated-on-white-background-design-vector.jpg",
+        descruption: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    },
+    {
+        id: "3",
+        title: "Blog Title",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/004/815/622/original/software-development-programming-language-coding-3d-isometric-laptop-computer-with-digital-application-isolated-on-white-background-design-vector.jpg",
+        descruption: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      },
+];
+const createBlog = (blog) => {
+  return `<div class="card blogCard" style="width: 18rem;" >
+    <img src="${blog.image}" alt="blog-image">
+    <div class="card-body">
+      <h5 class="card-title">${blog.title}</h5>
+      <p class="card-text">${blog.descruption}</p>
+      <a href="#" class="btn btn-primary">Read More</a>
+    </div>
+  </div>`;
+};
+
+const blogContainer = document.createElement("div");
+blogContainer.className = "blogContainer";
+document.body.appendChild(blogContainer);
+
+blogArray.forEach(blog => {
+  const blogDiv = document.createElement("div");
+  blogDiv.innerHTML = createBlog(blog);
+  blogContainer.appendChild(blogDiv);
+})
